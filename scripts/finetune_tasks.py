f73708fd6eb413760517629b248384526452ba62
Emma Rafkin <emmarafkin@gmail.com>
2025-10-22 20:42:08 -0400

    added wrong file

--- scripts/finetune_tasks.py
+++ scripts/finetune_tasks.py
@@ -200,20 +200,19 @@ def train_NER_model(model_checkpoint):
 
 def train_POS_model(model_checkpoint, GUM_folder: str = "GUM_en"):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-    data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
     train_dataset = load_conllu_data(f"{GUM_folder}/en_gum-ud-train.conllu")
     dev_dataset = load_conllu_data(f"{GUM_folder}/en_gum-ud-dev.conllu")
     dataset = DatasetDict({"train": train_dataset, "dev": dev_dataset})
-    unique_tags = sorted({tag for ex in dataset["train"] for tag in ex["pos_tags"]})
+    unique_tags = sorted({tag for ex in dataset["train"] for tag in ex.pos_tags})
     label2id = {tag: i for i, tag in enumerate(unique_tags)}
     id2label = {i: tag for tag, i in label2id.items()}
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+    data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
     def tokenize_and_align_labels(examples):
         # from https://reybahl.medium.com/token-classification-in-python-with-huggingface-3fab73a6a20e
-        tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+        tokenized_inputs = tokenizer(examples.tokens, truncation=True, is_split_into_words=True)
         labels = []
-        for i, label in enumerate(examples[f"pos_tags"]):
+        for i, label in enumerate(examples.pos_tags):
             word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
             previous_word_idx = None
             label_ids = []

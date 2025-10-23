e9fdf47739308f340f324beebce216ebc5a4d5ef
Emma Rafkin <emmarafkin@gmail.com>
2025-10-22 21:08:54 -0400

    pos test

--- scripts/POS_test.py
+++ scripts/POS_test.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from typing import List
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from transformers import AutoModelForMaskedLM, AutoTokenizer, AutoConfig, DataCollatorForTokenClassification
 from tqdm import tqdm
 from safetensors.torch import load_model
@@ -37,7 +37,8 @@ def lambda_search(eval_set, coef):
     ...
 
 def get_label_mapping():
-    pos_dataset = load_conllu_data(f"GUM_en/en_gum-ud-train.conllu")
+    pos_dataset = load_conllu_data("GUM_en/en_gum-ud-train.conllu")
+    pos_dataset = Dataset.from_pandas(pos_dataset)
     unique_tags = sorted({tag for ex in pos_dataset for tag in ex["pos_tags"]})
     label2id = {tag: i for i, tag in enumerate(unique_tags)}
     id2label = {i: tag for tag, i in label2id.items()}
@@ -45,17 +46,19 @@ def get_label_mapping():
 
 def test_lang_pos(ner, language_model, pretrained_checkpoint, language_folder, label2id, lambdas: List[float]= [0.0, 0.25, 0.5, 0.75, 1.0], batch_size:int=32):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    if language_folder != "en":
+    if language_model != "language_en_done":
         lv = get_language_vector(pretrained_checkpoint, language_model)
         best_lambda = 0.0
         ner = apply_language_vector_to_model(ner, lv, best_lambda) # TODO find best lambda:
-    NER_dataset = load_dataset("MultiCoNER/multiconer_v2", language_dataset, trust_remote_code=True)
+    test_dataset = load_conllu_data(language_folder)
+    test_dataset = Dataset.from_pandas(test_dataset)
+
     tokenizer = AutoTokenizer.from_pretrained(pretrained_checkpoint)
     def tokenize_and_align_labels(examples):
         # from https://reybahl.medium.com/token-classification-in-python-with-huggingface-3fab73a6a20e
         tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
         labels = []
-        for i, label in enumerate(examples[f"ner_tags"]):
+        for i, label in enumerate(examples[f"pos_tags"]):
             word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
             previous_word_idx = None
             label_ids = []
@@ -71,18 +74,17 @@ def test_lang_pos(ner, language_model, pretrained_checkpoint, language_folder, l
 
         tokenized_inputs["labels"] = labels
         return tokenized_inputs
-    tokenized_dataset= NER_dataset.map(
+    test_dataset= test_dataset.map(
         tokenize_and_align_labels,
         batched=True,
     )
     preds = []
     labels = []
     ner.to(device).eval()
-    test = tokenized_dataset["train"]
-    test.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
+    test_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
 
     collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
-    test_dataloader = DataLoader(test, batch_size=batch_size, collate_fn=collator)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=collator)
     with torch.no_grad():
         for batch in tqdm(test_dataloader):
             input_ids = batch["input_ids"].to(device)
@@ -96,7 +98,13 @@ def test_lang_pos(ner, language_model, pretrained_checkpoint, language_folder, l
     return accuracy
 
 if __name__ == "__main__":
-    datasets = ["English (EN)", "Spanish (ES)", "Hindi (HI)", "German (DE)", "Chinese (ZH)"]
+    datasets = [
+        "UD_English-PUD/en_pud-ud-test.conllu",
+        "UD_Spanish-PUD/es_pud-ud-test.conllu",
+        "UD_Hindi-PUD/hi_pud-ud-test.conllu",
+        "UD_German-PUD/de_pud-ud-test.conllu", 
+        "UD_Chinese-PUD/zh_pud-ud-test.conllu"
+        ]
     language_models = ["language_en_done", "language_es_done", "language_hi_done", "language_de_done", "language_zh_done"]
     id2label, label2id = get_label_mapping()
     encoder_checkpoint = "language_en_done"
@@ -113,7 +121,7 @@ if __name__ == "__main__":
     with open("output/POS_0.0.txt", "w") as f:
         for idx, model in enumerate(language_models):
             print("language model", model)
-            accuracy= test_lang_ner(ner_model, model, "language_en_done", datasets[idx], label2id)
+            accuracy= test_lang_pos(ner_model, model, "language_en_done", datasets[idx], label2id)
             print(f"accuracy: {accuracy}")  
             f.write(f"\n======language: {model.split('_')[1]}=======\n")
             f.write(f"accuracy: {accuracy}\n")

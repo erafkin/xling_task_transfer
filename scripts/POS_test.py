ba6d91d3138518e2ab664a79310455927b012c8b
Emma Rafkin <emmarafkin@gmail.com>
2025-10-22 21:14:35 -0400

    POS done

--- scripts/POS_test.py
+++ scripts/POS_test.py
@@ -48,7 +48,7 @@ def test_lang_pos(ner, language_model, pretrained_checkpoint, language_folder, l
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if language_model != "language_en_done":
         lv = get_language_vector(pretrained_checkpoint, language_model)
-        best_lambda = 0.0
+        best_lambda = 1.0
         ner = apply_language_vector_to_model(ner, lv, best_lambda) # TODO find best lambda:
     test_dataset = load_conllu_data(language_folder)
     test_dataset = Dataset.from_pandas(test_dataset)
@@ -118,7 +118,7 @@ if __name__ == "__main__":
     ner_model = TokenClassificationHead(bert_encoder, num_labels=len(id2label))
     load_model(ner_model, "POS_en/model.safetensors", device="cpu")
     print('pos model loaded')
-    with open("output/POS_0.0.txt", "w") as f:
+    with open("output/POS_1.0.txt", "w") as f:
         for idx, model in enumerate(language_models):
             print("language model", model)
             accuracy= test_lang_pos(ner_model, model, "language_en_done", datasets[idx], label2id)

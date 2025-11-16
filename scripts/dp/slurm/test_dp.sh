0a0239160858885f0d29f3b1106214f2a8b76acb
Emma Rafkin <emmarafkin@gmail.com>
2025-11-16 14:08:06 -0500

    dp test slurm but no slurm script lol

---
+++ scripts/dp/slurm/test_dp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name="test_dp"
+#SBATCH --nodes=1
+#SBATCH --partition=base
+#SBATCH --output="%x.o%j"
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --mem=10G
+#SBATCH --time=60:00:00
+
+module load cuda/12.5
+
+module load gcc/11.4.0
+ 
+python3.11 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3.11 --version
+python3.11 scripts/dp/DP_test.py 

7321c01d825405de5a52f3cc8b6d0152a474810e
Emma Rafkin <emmarafkin@gmail.com>
2025-11-16 14:06:12 -0500

    added slurm script

---
+++ scripts/dp/slurm/train_dp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name="train_dp"
+#SBATCH --nodes=1
+#SBATCH --partition=base
+#SBATCH --output="%x.o%j"
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --mem=10G
+#SBATCH --time=60:00:00
+#SBATCH --mail-user=epr41@georgetown.edu
+#SBATCH --mail-type=END,FAIL
+
+module load cuda/12.5
+
+module load gcc/11.4.0
+ 
+python3.11 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3.11 --version
+python3.11 ./scripts/pd/DP_train.py

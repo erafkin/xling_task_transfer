111327594f5d75173554ae2d788fcdf15c91f993
Emma Rafkin <emmarafkin@gmail.com>
2026-02-14 11:00:41 -0500

    NLI test

--- scripts/nli/NLI_test.py
+++ scripts/nli/NLI_test.py
@@ -2,7 +2,7 @@ import json
 import torch
 from torch.utils.data import DataLoader
 from datasets import load_dataset
-from transformers import AutoModelForSequenceClassification, AutoModelForMaskedLM, AutoTokenizer, DataCollatorWithPadding, AutoConfig, DataCollatorForTokenClassification, BertForSequenceClassification
+from transformers import AutoModelForSequenceClassification, AutoModelForMaskedLM, AutoTokenizer, DataCollatorWithPadding, AutoConfig, DataCollatorForTokenClassification, BertForSequenceClassification, AutoModelForCausalLM
 from tqdm import tqdm
 from scripts.task_vectors import TaskVector
 import gc
@@ -10,6 +10,11 @@ import gc
 def get_language_vector(base_model: str, saved_language: str):
     lang_vector = TaskVector(pretrained_model=AutoModelForMaskedLM.from_pretrained(base_model),
                              finetuned_model=AutoModelForMaskedLM.from_pretrained(saved_language, local_files_only=True))
+    return lang_vector
+
+def get_language_vector_causal(base_model: str, saved_language: str):
+    lang_vector = TaskVector(pretrained_model=AutoModelForCausalLM.from_pretrained(base_model),
+                             finetuned_model=AutoModelForCausalLM.from_pretrained(saved_language, local_files_only=True))
     return lang_vector
 
 def apply_language_vector_to_model(nli_model_checkpoint: str, language_vector:TaskVector, lambda_coef: float):
@@ -43,11 +48,48 @@ def test_lang_nli(nli, language_model, pretrained_checkpoint, dataset, best_lamb
     gc.collect()
     return accuracy
 
+
+
+
+def test_lang_nli_causal(nli, language_model, pretrained_checkpoint, dataset, best_lambda:float=1.0, batch_size:int=8):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_checkpoint, trust_remote_code=True, padding_side="left")
+    tokenizer.pad_token = tokenizer.eos_token
+    lv = get_language_vector_causal(pretrained_checkpoint, language_model)
+
+    nli = apply_language_vector_to_model(nli, lv, best_lambda)
+
+    preds = []
+    labels = []
+    nli.to(device).eval()
+    with torch.no_grad():
+        for data in tqdm(dataset):
+            prompt = f"Sentences: {' | '.join([data['premise'], data['hypothesis']])}.\n NLI:"
+            inputs = tokenizer(prompt, return_tensors="pt").to(device)
+            output_ids = nli.generate(
+                **inputs,
+                max_new_tokens=64,
+                do_sample=False
+            )
+            text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+            pred_tag = text.split("NLI:")[-1].strip()
+            preds.append(pred_tag)
+            labels.append(data['label'])
+    print("PREDICTIONS: ", preds[:5])
+    print("LABELS: ", labels[:5])
+    total = len(preds)
+    correct = sum(1 for pred, lab in zip(preds,labels) if pred == lab)
+    accuracy = correct / total if total > 0 else 0
+    nli.to("cpu")
+    del nli
+    gc.collect()
+    return accuracy
+
 if __name__ == "__main__":
     test_lambdas = [0.0, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1.0]
     model_base = "base_finetuned"
-    bert_values = [True, False]
-    
+    base_models = ["bert", "roberta"]
+    base_models = ["qwen"]    
     datasets = ["en", "es", "hi", "de", "zh", "fr","ru"]
     language_models = ["language_en_done", 
                     "language_es_done", 
@@ -61,68 +103,95 @@ if __name__ == "__main__":
     torch.set_grad_enabled(False)  
     for idx, model in enumerate(language_models):
         overall_hyperparameter_results[model] = {}
-        for bert in bert_values:
-            overall_hyperparameter_results[model]["bert" if bert else "roberta"] = {}
-            if bert:
+        for base_model_str in base_models:
+            overall_hyperparameter_results[base_model_str] = {}
+            if base_model_str == "bert":
                 base_model = "google-bert/bert-base-multilingual-cased"
                 prefix = "bert-multilingual"
-            else:
+            elif base_model_str == "roberta":
                 base_model = "FacebookAI/xlm-roberta-base"
                 prefix = "xlm-roberta"
+            else:
+                base_model = "Qwen/Qwen3-0.6B"
+                prefix = "qwen"
             # handle data
             NLI_dataset = load_dataset("facebook/xnli", datasets[idx], trust_remote_code=True)
             tokenizer = AutoTokenizer.from_pretrained(base_model)
-            def preprocess(examples):
-                inputs = [examples[column] for column in ["premise", "hypothesis"]]
-                tokenized_examples = tokenizer(
-                    *inputs,
-                    padding="max_length",          # **important** – matches training
-                    truncation=True,
-                    max_length=512,
-                    return_tensors="pt",
-                )
-                tokenized_examples['label'] = examples["label"]
-                return tokenized_examples
-            tokenized_dataset= NLI_dataset.map(
-                preprocess,
-                batched=True,
-                remove_columns=NLI_dataset["test"].column_names
-            )
 
-            hyperparameter_results = {}
-            batch_size = 32
-            torch.set_grad_enabled(False)
-            for l in test_lambdas:
-                nli = AutoModelForSequenceClassification.from_pretrained(
-                    f"{prefix}/{model_base}/NLI_en",
-                    local_files_only=True,
-                )
-                nli.eval()
-                accuracy = test_lang_nli(nli, f"{prefix}/{model}", base_model, tokenized_dataset["validation"], l, batch_size)
-                hyperparameter_results[l] = accuracy
-            print("hyperparamter serach results")
-            print(hyperparameter_results)
-            overall_hyperparameter_results[model]["bert" if bert else "roberta"] = hyperparameter_results
+            if base_model_str == "qwen":
+                hyperparameter_results = {}
+                nli =  AutoModelForCausalLM.from_pretrained(f"{prefix}/{model_base}/NLI_en")
+                for l in test_lambdas:
+                    print("lambda: ", l)
+                    accuracy = test_lang_nli_causal(nli, f"{prefix}/{model}", base_model, NLI_dataset["validation"], l)
+                    hyperparameter_results[l] = accuracy
+                print("hyperparamter search results")
+                print(hyperparameter_results)
+                overall_hyperparameter_results[model][base_model_str] = hyperparameter_results
+                best_lambda = max(hyperparameter_results, key=hyperparameter_results.get)
+                print(best_lambda)
+                nli =  AutoModelForCausalLM.from_pretrained(f"{prefix}/{model_base}/NLI_en")
+                with open(f"output/{prefix}/{model_base}/NLI.txt", "a") as f:
+                    print("language model", model)
+                    accuracy= test_lang_nli_causal(nli, f"{prefix}/{model}", base_model, NLI_dataset["test"], best_lambda)
+                    print(f"accuracy: {accuracy}")  
+                    f.write(f"\n======language: {model.split('_')[1]}=======\n")
+                    f.write(f"best lambda: {best_lambda}\n")
+                    f.write(f"accuracy: {accuracy}\n")
+                    f.close()
 
-            best_lambda = max(hyperparameter_results, key=hyperparameter_results.get)
-            print(best_lambda)
-            with open(f"output/{prefix}/{model_base}/NLI.txt", "a") as f:
-                print("language model", model)
-                nli = AutoModelForSequenceClassification.from_pretrained(
-                    f"{prefix}/{model_base}/NLI_en",
-                    local_files_only=True
+            else:
+                def preprocess(examples):
+                    inputs = [examples[column] for column in ["premise", "hypothesis"]]
+                    tokenized_examples = tokenizer(
+                        *inputs,
+                        padding="max_length",          # **important** – matches training
+                        truncation=True,
+                        max_length=512,
+                        return_tensors="pt",
+                    )
+                    tokenized_examples['label'] = examples["label"]
+                    return tokenized_examples
+                tokenized_dataset= NLI_dataset.map(
+                    preprocess,
+                    batched=True,
+                    remove_columns=NLI_dataset["test"].column_names
                 )
 
-                nli.eval()                         # disables dropout / batch‑norm  
-                accuracy= test_lang_nli(nli, f"{prefix}/{model}", base_model, tokenized_dataset["test"], best_lambda, batch_size)
-                print(f"accuracy: {accuracy}")  
-                f.write(f"\n======language: {model.split('_')[1]}=======\n")
-                f.write(f"best lambda: {best_lambda}\n")
-                f.write(f"accuracy: {accuracy}\n")
+                hyperparameter_results = {}
+                batch_size = 32
+                torch.set_grad_enabled(False)
+                for l in test_lambdas:
+                    nli = AutoModelForSequenceClassification.from_pretrained(
+                        f"{prefix}/{model_base}/NLI_en",
+                        local_files_only=True,
+                    )
+                    nli.eval()
+                    accuracy = test_lang_nli(nli, f"{prefix}/{model}", base_model, tokenized_dataset["validation"], l, batch_size)
+                    hyperparameter_results[l] = accuracy
+                print("hyperparamter serach results")
+                print(hyperparameter_results)
+                overall_hyperparameter_results[model][base_model_str] = hyperparameter_results
+
+                best_lambda = max(hyperparameter_results, key=hyperparameter_results.get)
+                print(best_lambda)
+                with open(f"output/{prefix}/{model_base}/NLI.txt", "a") as f:
+                    print("language model", model)
+                    nli = AutoModelForSequenceClassification.from_pretrained(
+                        f"{prefix}/{model_base}/NLI_en",
+                        local_files_only=True
+                    )
+
+                    nli.eval()                         # disables dropout / batch‑norm  
+                    accuracy= test_lang_nli(nli, f"{prefix}/{model}", base_model, tokenized_dataset["test"], best_lambda, batch_size)
+                    print(f"accuracy: {accuracy}")  
+                    f.write(f"\n======language: {model.split('_')[1]}=======\n")
+                    f.write(f"best lambda: {best_lambda}\n")
+                    f.write(f"accuracy: {accuracy}\n")
+                    f.close()
+        with open(f"output/{base_model_str}/base_finetuned/NLI_pretrained_hyperparameter_search_{base_model_str}.json", "w") as f:
+                json.dump(overall_hyperparameter_results, f, indent=4)
                 f.close()
-    with open(f"output/NLI_pretrained_hyperparameter_search.json", "w") as f:
-        json.dump(overall_hyperparameter_results, f, indent=4)
-        f.close()
 
 
 
